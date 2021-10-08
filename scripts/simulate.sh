e902dcb182ebee4044ba9b733ac44e8073adf3d8
Ziqi Xu <57846897+ziqixu091@users.noreply.github.com>
2021-10-08 19:51:08 +0800

    formatting

--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -5,15 +5,19 @@
 #$ -o ./job_out
 
 PLINK_DIR=/u/project/pasaniuc/pasaniucdata/admixture/projects/admix-prs-uncertainty/data/PLINK
+cur_dir="$(pwd)"
 
 #chr_i=$((SGE_TASK_ID))
 chr_i=$1
-var_g=$2
-var_e=$3
+h2=$2
+cau_prop=$3
+
+mkdir -p /out/chr{$chr_i}_h{$h2}_cau{$cau_prop}/
 
 python3 simulate.py \
     --data_dir ${PLINK_DIR} \
     --chr_i ${chr_i} \
-    --var_g ${var_g} \
-    --var_e ${var_e} 
+    --h2 ${h2} \
+    --cau_prop ${cau_prop} \
+    --out ./out/chr${chr_i}_h2_${h2}_cau_${cau_prop}/
 
